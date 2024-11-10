98b3e04ef4f7ac8d7b8b9ba505732c07f2010b47
zaneObwoni <zaneobwoni@gmail.com>
2024-11-10 02:01:39 +0100

    prettier fmt

--- apps/the-black-pearl/app/(tabs)/_layout.tsx
+++ apps/the-black-pearl/app/(tabs)/_layout.tsx
@@ -1,38 +1,38 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from 'expo-router'
+import React from 'react'
 
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { SymbolView } from 'expo-symbols';
+import { Colors } from '@/constants/Colors'
+import { useColorScheme } from '@/hooks/useColorScheme'
+import { SymbolView } from 'expo-symbols'
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme()
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}
-    >
-      <Tabs.Screen
-        name="play"
-        options={{
-          title: 'play',
-          tabBarIcon: ({ color, focused }) => (
-            <SymbolView name="livephoto.play" />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="players"
-        options={{
-          title: 'players',
-          tabBarIcon: ({ color, focused }) => (
-            <SymbolView name="person.2.crop.square.stack" />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+                headerShown: false,
+            }}
+        >
+            <Tabs.Screen
+                name="play"
+                options={{
+                    title: 'play',
+                    tabBarIcon: ({ color, focused }) => (
+                        <SymbolView name="livephoto.play" />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="players"
+                options={{
+                    title: 'players',
+                    tabBarIcon: ({ color, focused }) => (
+                        <SymbolView name="person.2.crop.square.stack" />
+                    ),
+                }}
+            />
+        </Tabs>
+    )
 }
